bc443afd93cc0e509d3df3281bb6ed8286bcc703
Wei Zhu <zhuw10@nih.gov>
2021-03-09 00:56:42 +0000

    minor change

--- igv_snapshot_maker/cli.py
+++ igv_snapshot_maker/cli.py
@@ -6,7 +6,7 @@ import argparse
 import warnings
 import yaml
 
-from igv_snapshot_maker.igv_snapshot_maker import IGV_Snapshot_Maker
+from igv_snapshot_maker import IGV_Snapshot_Maker
 
 '''
 Ref: https://github.com/stevekm/IGV-snapshot-automator/blob/master/make_IGV_snapshots.py
